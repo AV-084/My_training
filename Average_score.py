a53ea6329a58225f5109c7423c26293638948a39
AV-084 <vorotnikof1109@gmail.com>
2024-09-03 20:37:53 +0300

    Add files via upload

--- Average_score.py
+++ Average_score.py
@@ -1,13 +1,6 @@
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_list = list (students)
-print (students_list)
-
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-a = [sum (grades[0])/len (grades[0]), sum (grades[1])/len (grades[1]), sum (grades[2])/len (grades[2]), sum (grades[3])/len (grades[3]), sum (grades[4])/len (grades[4])]
-print(a)
-b = set(a)
-print(set(b))
-
-
-zipped = zip (students_list, b)
-print(zipped)
+grades_averege = [sum (grades[0])/len (grades[0]), sum (grades[1])/len (grades[1]), sum (grades[2])/len (grades[2]), sum (grades[3])/len (grades[3]), sum (grades[4])/len (grades[4])]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_sorted = sorted(students)
+students_dict = dict (zip (students_sorted, grades_averege))
+print(students_dict)
