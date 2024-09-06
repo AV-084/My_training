c4330ce4dca4fe5c9ae3870044521c8815a08292
AV-084 <vorotnikof1109@gmail.com>
2024-09-06 23:08:19 +0300

    Update module_2_3.py
    While_break_continue

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-n = 0
-while n >= 0:
-    print (my_list[n])
+n = -1
+while n >= -1:
     n = n + 1
     if my_list[n] < 0 or n == len(my_list):
         break
+    if my_list[n] == 0:
+        continue
+    print (my_list[n])
